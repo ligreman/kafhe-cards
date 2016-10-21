3d5f1f3f2bb4b4fc17ff43407bdf1facd47789dc
Ligre <ligreman@users.noreply.github.com>
2016-10-22 00:16:14 +0200

    Planning

--- frontend/src/main/app/routes/explore/exploreController.js
+++ frontend/src/main/app/routes/explore/exploreController.js
@@ -16,41 +16,48 @@
                     function updateDone() {
                         // Calculo el ancho del div contenedor #map-container
                         $timeout(function () {
-                            console.log(document.getElementById('map-container').offsetWidth);
-                            console.log(document.getElementById('map-container').offsetHeight);
+                            var w = document.getElementById('map-container').offsetWidth,
+                                h = document.getElementById('map-container').offsetHeight;
+
+                            // Pongo al div del mapa el tamaño correcto
+                            document.getElementById('mapid').style.width = w + 'px';
+                            document.getElementById('mapid').style.height = h + 'px';
+
+                            var map = L.map('mapid', {
+                                // center: [51.505, -0.09],
+                                maxBounds: [[5, -180], [-81, 65]],
+                                sleep: false
+                                /*sleep: true,
+                                 sleepTime: 750,
+                                 wakeTime: 750,
+                                 sleepNote: true,
+                                 // should hovering wake the map? (non-touch devices only)
+                                 hoverToWake: false,
+                                 // a message to inform users about waking the map
+                                 wakeMessage: 'Click to Wake',
+                                 // a constructor for a control button
+                                 // sleepButton: L.Control.sleepMapControl,
+                                 // opacity for the sleeping map
+                                 sleepOpacity: .7*/
+                            }).setView([-70, -80], 3);
+
+                            L.tileLayer('assets/img/map/{z}/{x}/{y}.jpg', {
+                                minZoom: 3,
+                                maxZoom: 6,
+                                tms: true
+                            }).addTo(map);
+
+                            var marker = L.marker([-47.5, 0]).addTo(map);
+                            marker = L.marker([-57.5, 0]).addTo(map);
+                            marker = L.marker([-67.5, 0]).addTo(map);
+                            marker = L.marker([-77.5, 0]).addTo(map);
+                            marker = L.marker([-87.5, 0]).addTo(map);
+
+                            marker = L.marker([5, -180]).addTo(map);
                         }, 0);
 
                         //TODO centrarlo en la posición del jugador
-                        /*var map = L.map('mapid', {
-                         // center: [51.505, -0.09],
-                         maxBounds: [[5, -180], [-80, 85]],
-                         sleep: true,
-                         sleepTime: 750,
-                         wakeTime: 750,
-                         sleepNote: true,
-                         // should hovering wake the map? (non-touch devices only)
-                         hoverToWake: false,
-                         // a message to inform users about waking the map
-                         wakeMessage: 'Click to Wake',
-                         // a constructor for a control button
-                         // sleepButton: L.Control.sleepMapControl,
-                         // opacity for the sleeping map
-                         sleepOpacity: .7
-                         }).setView([-70, -80], 3);
 
-                         L.tileLayer('assets/img/map/{z}/{x}/{y}.jpg', {
-                         minZoom: 3,
-                         maxZoom: 6,
-                         tms: true
-                         }).addTo(map);
-
-                         var marker = L.marker([-47.5, 0]).addTo(map);
-                         marker = L.marker([-57.5, 0]).addTo(map);
-                         marker = L.marker([-67.5, 0]).addTo(map);
-                         marker = L.marker([-77.5, 0]).addTo(map);
-                         marker = L.marker([-87.5, 0]).addTo(map);
-
-                         marker = L.marker([5, -180]).addTo(map);*/
 
                         /*var polyline = L.polyline([...]).addTo(map);
                          var decorator = L.polylineDecorator(polyline, {
